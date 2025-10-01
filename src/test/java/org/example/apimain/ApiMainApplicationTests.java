b60efa0c21eecc76824a02f0cc1432e01cad4b9b
vova4ek <vovan76126@gmail.com>
2025-10-01 22:45:34 +0300

    Create project

---
+++ src/test/java/org/example/apimain/ApiMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apimain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
