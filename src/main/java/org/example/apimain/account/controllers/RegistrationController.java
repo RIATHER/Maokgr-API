c6a6e48cd1ba86bee356d7d662e1676736696247
vova4ek <vovan76126@gmail.com>
2025-10-02 22:54:10 +0300

    Просто по приколу

--- src/main/java/org/example/apimain/account/controllers/RegistrationController.java
+++ src/main/java/org/example/apimain/account/controllers/RegistrationController.java
@@ -1,6 +1,9 @@
 package org.example.apimain.account.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.example.apimain.account.DTO.AccountDTO;
+import org.example.apimain.account.entities.AccountEntity;
+import org.example.apimain.account.service.RegistrationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/account")
 public class RegistrationController {
-
+    private final RegistrationService registrationService;
     // Запрос на регистрацию
     @PostMapping("/registration")
-    public ResponseEntity<AccountDTO> Registration(@RequestBody AccountDTO accountDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(accountDTO);
+    public ResponseEntity<AccountEntity> Registration(@RequestBody AccountDTO accountDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(registrationService.registrationAccount(accountDTO));
     }
 }
