f092e2b73bc586ed6732ecd06eaa0e8ce9ba108e
vova4ek <vovan76126@gmail.com>
2025-10-03 14:06:34 +0300

    Добавил настройки nullable, unique и т.д. + автоматическая генерация id через монотонный ULID перед сохранением в БД

--- src/main/java/org/example/apimain/account/entities/AccountEntity.java
+++ src/main/java/org/example/apimain/account/entities/AccountEntity.java
@@ -1,9 +1,12 @@
 package org.example.apimain.account.entities;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import org.example.apimain.account.enums.AccountStatus;
+import org.hibernate.annotations.JdbcTypeCode;
+import com.github.f4b6a3.ulid.UlidCreator;
+import org.hibernate.type.SqlTypes;
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
 
 import java.util.UUID;
 
@@ -12,21 +15,29 @@ import java.util.UUID;
 @Getter
 @Setter
 public class AccountEntity {
-
     @Id
-    @Column(name = "id")
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
     private UUID id;
 
-    @Column(name = "username")
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password_hash")
+    @Column(name = "password_hash", nullable = false)
     private String password;
 
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", columnDefinition = "account_status")
-    private AccountStatus status;
+    @Column(name = "status", nullable = false)
+    private AccountStatus status = AccountStatus.ACTIVE;
+
+    // Добавление перед сохранением в БД
+    @PrePersist
+    public void prePersist(){
+        if (id == null){
+            this.id = UlidCreator.getMonotonicUlid().toUuid();
+        }
+    }
 }
