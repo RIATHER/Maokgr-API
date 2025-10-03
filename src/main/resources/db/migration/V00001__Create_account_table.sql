0bee372ed23ea680bb91911e992648e47c75daa9
vova4ek <vovan76126@gmail.com>
2025-10-03 14:07:41 +0300

    Поменял немного таблица, для миграции БД.

--- src/main/resources/db/migration/V00001__Create_account_table.sql
+++ src/main/resources/db/migration/V00001__Create_account_table.sql
@@ -1,9 +1,9 @@
 CREATE TYPE account_status AS ENUM ('ACTIVE', 'BLOCKED');
 
 CREATE TABLE accounts (
-    id UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    status account_status NOT NULL DEFAULT 'ACTIVE'
+    status account_status NOT NULL
 );
