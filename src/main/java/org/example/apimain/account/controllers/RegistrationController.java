9fcfd1d297fd645c5bcde6775deb94a20704a972
vova4ek <vovan76126@gmail.com>
2025-10-03 14:07:20 +0300

    Добавил Логгер

--- src/main/java/org/example/apimain/account/controllers/RegistrationController.java
+++ src/main/java/org/example/apimain/account/controllers/RegistrationController.java
@@ -1,24 +1,28 @@
 package org.example.apimain.account.controllers;
 
-import lombok.RequiredArgsConstructor;
-import org.example.apimain.account.DTO.AccountDTO;
-import org.example.apimain.account.entities.AccountEntity;
 import org.example.apimain.account.service.RegistrationService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.apimain.account.entities.AccountEntity;
+import org.example.apimain.account.DTO.AccountDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/account")
 public class RegistrationController {
     private final RegistrationService registrationService;
+    private final Logger log = LoggerFactory.getLogger(RegistrationController.class);
     // Запрос на регистрацию
     @PostMapping("/registration")
     public ResponseEntity<AccountEntity> Registration(@RequestBody AccountDTO accountDTO) {
+        log.info("Запрос на регистрацию аккаунта");
         return ResponseEntity.status(HttpStatus.CREATED).body(registrationService.registrationAccount(accountDTO));
     }
 }
