9253e04fa3df37185fddd634eccb53baa2e4d1de
vova4ek <vovan76126@gmail.com>
2025-10-03 14:06:52 +0300

    Просто исправил

--- src/main/java/org/example/apimain/account/repositories/AccountRepository.java
+++ src/main/java/org/example/apimain/account/repositories/AccountRepository.java
@@ -2,7 +2,9 @@ package org.example.apimain.account.repositories;
 
 import org.example.apimain.account.entities.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<AccountEntity,String>{
 
 }
