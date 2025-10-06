2894df94ed32e9acccac88ac0f823074e8711f5a
vova4ek <vovan76126@gmail.com>
2025-10-06 21:43:32 +0300

    :C

--- src/main/java/org/example/apimain/account/controllers/RegistrationController.java
+++ src/main/java/org/example/apimain/account/controllers/RegistrationController.java
@@ -10,6 +10,7 @@ import org.example.apimain.account.DTO.AccountDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -21,7 +22,7 @@ public class RegistrationController {
     private final Logger log = LoggerFactory.getLogger(RegistrationController.class);
     // Запрос на регистрацию
     @PostMapping("/registration")
-    public ResponseEntity<AccountEntity> Registration(@RequestBody AccountDTO accountDTO) {
+    public ResponseEntity<AccountEntity> Registration(@Valid @RequestBody AccountDTO accountDTO) {
         log.info("Запрос на регистрацию аккаунта");
         return ResponseEntity.status(HttpStatus.CREATED).body(registrationService.registrationAccount(accountDTO));
     }
