d3a31a9e5634ee434d1835035d8f57cca576e7bd
vova4ek <vovan76126@gmail.com>
2025-10-06 21:45:03 +0300

    Реализована проверка дубликатов полей в AccountDTO с DuplicateFieldException

--- src/main/java/org/example/apimain/account/service/RegistrationService.java
+++ src/main/java/org/example/apimain/account/service/RegistrationService.java
@@ -5,7 +5,11 @@ import org.example.apimain.account.DTO.AccountDTO;
 import org.example.apimain.account.entities.AccountEntity;
 import org.example.apimain.account.repositories.AccountRepository;
 import org.example.apimain.account.utils.AccountMapper;
+import org.example.apimain.account.utils.exception.DuplicateFieldException;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,20 @@ public class RegistrationService {
     private final AccountMapper accountMapper;
 
     public AccountEntity registrationAccount(AccountDTO accountDTO) {
-        return accountMapper.toEntity(accountDTO);
+        validateRegistration(accountDTO);
+        return accountRepository.save(accountMapper.toEntity(accountDTO));
+    }
+
+    protected void validateRegistration(AccountDTO accountDTO) {
+        DuplicateFieldException duplicateFieldException = new DuplicateFieldException();
+        Stream.of(
+            Map.entry("username", accountRepository.existsByUsername(accountDTO.getUsername())),
+            Map.entry("email", accountRepository.existsByEmail(accountDTO.getEmail())))
+        .filter(Map.Entry::getValue).forEach(entry ->
+             duplicateFieldException.addFieldError(entry.getKey(), "Уже существует")
+        );
+        if (!duplicateFieldException.getDuplicateFields().isEmpty()){
+            throw duplicateFieldException;
+        }
     }
 }
